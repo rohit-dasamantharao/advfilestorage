497cd8221af2128c59490c32f925416b3b548d7d
ddrohit <dasamantharao.rohit@gmail.com>
2022-08-30 18:53:47 +0530

    added share, loading option while file uopload

--- dist/public/js/index.js
+++ dist/public/js/index.js
@@ -1,17 +1,45 @@
+
 const Upload = async function() {
 
     let fileupload = document.getElementById("fileupload");
     let formData = new FormData();           
     formData.append("upl", fileupload.files[0]);
-    let response = await fetch('/upload', {
-      method: "POST", 
-      body: formData
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/upload", true);
+
+    xhr.upload.addEventListener('progress',function(e) {
+      var percentComplete = Math.ceil((e.loaded / e.total) * 100);
+      document.getElementById("Uploadresult").className = "showblock";
+      document.getElementById("sharelink").innerHTML = `Uploading.... ${percentComplete} %`;
     });
-    let data = await response.json();
-    console.log(data);
-    document.getElementById("sharelink").innerHTML = `${window.location.origin}/key=${data.urls[0].name}`;
-    document.getElementById("Uploadresult").className = "showblock";
-    alert('The file has been uploaded successfully.');
+
+    xhr.addEventListener("load",function() {
+      let data = JSON.parse(this.response);
+      document.getElementById("sharelink").innerHTML = `${window.location.origin}?key=${data.urls[0].name}`;
+      document.getElementById("Uploadresult").className = "showblock";
+
+      const shareData = {
+        title: data.urls[0].name,
+        text: `Sharing ${data.urls[0].name} via advance file storage`,
+        url: `${window.location.origin}?key=${data.urls[0].name}`
+      }
+      
+      const btn = document.getElementById('Share');
+      
+      // Share must be triggered by "user activation"
+      btn.addEventListener('click', async () => {
+        try {
+          await navigator.share(shareData);
+        } catch (err) {
+          alert("Failed to share")
+          console.log(`Error: ${err}`);
+        }
+      });
+
+      alert('The file has been uploaded successfully.');
+    });
+    xhr.send(formData);
 }
 
 
@@ -19,7 +47,7 @@ const Upload = async function() {
 const params = new URLSearchParams(window.location.search)
 if(params.has("key"))
 {
-    let filename =  params.get("key").split(/\d+-/)[1];
+    const filename = params.get("key").slice(params.get("key").indexOf('-') + 1)
     document.getElementById("downloadfile").className = "showblock";
     document.getElementById("downloadbutton").setAttribute("value",`Download ${filename}`)
     document.getElementById("downloadbutton").addEventListener("click",()=>{
@@ -31,7 +59,7 @@ if(params.has("key"))
         const url = window.URL.createObjectURL(newBlob);
        const link = document.createElement('a');
        link.href = url;
-       link.setAttribute('download', filename);
+       link.setAttribute('download', filename.concat(''));
        document.body.appendChild(link);
        link.click();
       })
